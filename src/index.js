1c051fa2cdc743a6c28083bae83b6eaf9c555a8a
Basant Bansal <itsbasanthere@gmail.com>
2026-02-24 14:15:47 +0530

    Added utis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./add.js"
 
 dotenv.config({
     path:"./env"
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Error connecting to database",err)
+})
